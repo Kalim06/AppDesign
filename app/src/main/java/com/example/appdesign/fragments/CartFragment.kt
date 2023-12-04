ef25a2d1832e9b77771ff79035340f29bf619567
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-04 22:32:07 +0530

    Cart Navigation

--- app/src/main/java/com/example/appdesign/fragments/CartFragment.kt
+++ app/src/main/java/com/example/appdesign/fragments/CartFragment.kt
@@ -5,14 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.appdesign.R
+import androidx.navigation.findNavController
+import com.example.appdesign.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
+
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cartBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
     }
 }
