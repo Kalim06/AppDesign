6e081a9f8770587665732638df834ba7475bac78
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-04 22:00:41 +0530

    Home Navigation

--- app/src/main/java/com/example/appdesign/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/appdesign/fragments/HomeFragment.kt
@@ -5,14 +5,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.appdesign.R
+import com.example.appdesign.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.homeUserImage.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+
+        binding.searchBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+        }
+
+        binding.listItem1.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        binding.listItem2.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        binding.listItem3.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        binding.featuredItem1.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        binding.featuredItem2.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        binding.featuredItem3.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        binding.featuredItem4.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
     }
 }
