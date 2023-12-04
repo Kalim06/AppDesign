2235fa50aed753cf699afaef61469202300b9724
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-04 22:29:12 +0530

    Search Navigation

--- app/src/main/java/com/example/appdesign/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/appdesign/fragments/SearchFragment.kt
@@ -5,14 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.appdesign.R
+import com.example.appdesign.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+
+        binding.searchCart.setOnClickListener {
+            it.findNavController().navigate(R.id.action_searchFragment_to_cartFragment)
+        }
+
+        binding.searchItem1.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_searchFragment_to_productDetailFragment)
+        }
+
+        binding.searchItem2.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_searchFragment_to_productDetailFragment)
+        }
+
+        binding.searchItem3.root.setOnClickListener {
+            it.findNavController().navigate(R.id.action_searchFragment_to_productDetailFragment)
+        }
+
     }
 }
