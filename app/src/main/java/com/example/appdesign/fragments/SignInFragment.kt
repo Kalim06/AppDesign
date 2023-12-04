ce2efc7a6c1e71ef421650956db698e404733bef
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-04 21:20:17 +0530

    Sign In Navigation

--- app/src/main/java/com/example/appdesign/fragments/SignInFragment.kt
+++ app/src/main/java/com/example/appdesign/fragments/SignInFragment.kt
@@ -5,14 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.appdesign.R
+import com.example.appdesign.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Sign In
+        binding.signInBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+        }
+
+        //Sign Up
+        binding.signUpDirectBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
     }
 }
