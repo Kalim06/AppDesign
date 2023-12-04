b4966404bceb96ba5ab00e56cab96572b12a78d4
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-04 17:46:44 +0530

    Design
    Design Completed

--- app/src/main/java/com/example/appdesign/MainActivity.kt
+++ app/src/main/java/com/example/appdesign/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.appdesign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
     }
 }
