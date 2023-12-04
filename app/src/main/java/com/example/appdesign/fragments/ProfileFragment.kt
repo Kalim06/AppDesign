557964b26079caf381e053588a4687ce68f6a7ac
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-04 22:08:37 +0530

    Profile Navigation

--- app/src/main/java/com/example/appdesign/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/appdesign/fragments/ProfileFragment.kt
@@ -5,14 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.appdesign.R
+import com.example.appdesign.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.profileBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+
+        binding.logOut.setOnClickListener {
+            it.findNavController().navigate(R.id.action_profileFragment_to_signInFragment)
+        }
+
     }
 }
